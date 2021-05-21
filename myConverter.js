e53a4c62984262c2c7bc0c60727dd215bf752471
oluwatobi <oluwatobi275@gmail.com>
2021-05-21 13:54:17 +0100

    test work

--- myConverter.js
+++ myConverter.js
@@ -3,27 +3,27 @@ let convertFahrToCelsius = (faht) => {
 
         let celsius = faht - 32 * (5 / 9);
         console.log(celsius.toFixed(4));
-        //return celsius.toFixed(4);
+        return celsius.toFixed(4);
     } else if (typeof faht == 'object') {
         if (Array.isArray(faht)) {
             
             console.log (`${JSON.stringify(faht)} is not a valid number but an Array`);
 
-           // return `${$JSON.stringify(faht)} is not a valid number but an Array`;
+           return `${$JSON.stringify(faht)} is not a valid number but an Array`;
         } else {
             let a = faht;
             console.log (`${JSON.stringify(faht)}  is not a valid number but a ${typeof faht}. type`)
 
-            //display = `${JSON.stringify(faht)} is not a valid number but a ${typeof faht}.`
+            display = `${JSON.stringify(faht)} is not a valid number but a ${typeof faht}.`
              
-            //return display;
+            return display;
         
         }
     } else if (typeof faht == 'string') {
         let fahtNumber = Number(faht);
         if (isNaN(fahtNumber)) {
             console.log (`${faht} is not a valid number but a ${typeof faht}. type`)
-            //return `${faht} is not a valid number but a ${typeof faht}. type`
+            return `${faht} is not a valid number but a ${typeof faht}. type`
         } else {
             let celsius = fahtNumber - 32 * (5 / 9);
             console.log (celsius.toFixed(4))
@@ -31,9 +31,6 @@ let convertFahrToCelsius = (faht) => {
     } else {
         
         console.log(typeof faht);
-
-
-
     }
 }
 
